9cb5bcf7b8ea0ff337030b52bba2651bbb00df96
ashish <144021866+ashishexee@users.noreply.github.com>
2025-08-26 19:11:17 +0530

    Update vite.config.js

--- game_onechain/vite.config.js
+++ game_onechain/vite.config.js
@@ -1,7 +1,24 @@
-export default {
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
   base: './',
   server: {
     open: true,
-    port : 5174
+    port: 5174
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      'assets': resolve(__dirname, 'assets')
+    }
+  },
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
   }
-};
+});
